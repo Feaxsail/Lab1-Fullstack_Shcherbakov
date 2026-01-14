1079add91ec12fca9a632e3050213e8be6b049a7
Nadont02 <dunyushkin2007@mail.ru>
2026-01-14 09:53:59 +0300

    1

---
+++ backend/program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вывод приветствия
+            Console.WriteLine("Здорова!");
+
+            // Вывод ФИО (замените на своё)
+            Console.WriteLine("Щербаков Данил Николаевич");
+
+            // Вывод текущей даты и времени
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
